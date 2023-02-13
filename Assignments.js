a1aa85bb9806e287e70fc7068163a1b07dd22f9c
PYadav19 <poojaydm9@gmail.com>
2023-02-13 16:54:50 +0530

    assignment

---
+++ Assignments.js
@@ -0,0 +1,14 @@
+let country = "India";
+// let continent = "Asia";
+let population = "1,415,347,996";
+//console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// DATATYPES ASSIGNMENT
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof country);
+conaole.log(typeof language);
+console.log(typeof population);
---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+let js = "amazing";
+if (js === "amazing") alert("Javascript is Fun!");
+
+console.log(40 + 20 - 25);
+*/
+
+// DATA TYPES - NUMBER , BOOLEAN , STRING , UNDEFINED, NULL
+// // typeof is a function which tell the type of the value
+// true;
+// console.log(true); // boolean
+// let javaScriptIsFun = true; //boolean
+// console.log(javaScriptIsFun);
+// console.log(23); //Number
+// console.log("Pooja"); //string
+
+// console.log(typeof javaScriptIsFun);
+
+// // Undefined
+// let firstName;
+// console.log(typeof firstName);
+
+// // Dynamic typing -> chnaging the type of value
+
+// javaScriptIsFun = "Yes!";
+// console.log(typeof javaScriptIsFun);
