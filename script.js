91fd9aa6943170bf6f57bbe25a78b1b468f783a0
PYadav19 <poojaydm9@gmail.com>
2023-02-15 18:03:16 +0530

    coding challenge1

--- script.js
+++ script.js
@@ -39,35 +39,103 @@ birthYear = 1998; // this will give error - "Assignment to constant variable"
 // OPERATORS  -> USING minus operator to calculate the age of person using birthyear.
 // Formula -> variable name = current year - birthYear
 
-const now = 2023;
-const ageVishali = now - 1997;
-const ageKashish = now - 1998;
-console.log(ageVishali, ageKashish); // we can log multiple values and expressions
+// const now = 2023;
+// const ageVishali = now - 1997;
+// const ageKashish = now - 1998;
+// console.log(ageVishali, ageKashish); // we can log multiple values and expressions
 
-console.log(ageKashish * 2, ageVishali / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 -> 2 * 2* 2
+// console.log(ageKashish * 2, ageVishali / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 -> 2 * 2* 2
 
 //  + operator can be used for joining 2 strings or we can say concatination apart from addition
 
-const firstName = "Pooja";
-const lastName = "Yadav";
-console.log(firstName + " " + lastName);
+// const firstName = "Pooja";
+// const lastName = "Yadav";
+// console.log(firstName + " " + lastName);
 
 // typeoff operator tells the type of the value
 
 // ASSIGNMENT OPERATORS -> = , += , *= , ++ , --
-let x = 5 + 10;
-x += 10; // x = x + 10
-x *= 4; // x = x * 4
-x++; //  x = x + 1
-x--; // x = x - 1
-console.log(x);
+// let x = 5 + 10;
+// x += 10; // x = x + 10
+// x *= 4; // x = x * 4
+// x++; //  x = x + 1
+// x--; // x = x - 1
+// console.log(x);
 
-// COMPARISION OPERATOR
+// // COMPARISION OPERATOR
 
-console.log(ageKashish < ageVishali); // < , > , <= ,>=
-console.log(ageKashish >= 25);
+// console.log(ageKashish < ageVishali); // < , > , <= ,>=
+// console.log(ageKashish >= 25);
 
-const isFullAge = ageKashish >= 25;
+// const isFullAge = ageKashish >= 25;
 
-console.log(now - 1997 > now - 1998);
+// console.log(now - 1997 > now - 1998);
+
+// Coding Challenge 1
+
+// Test Data 1
+
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 92;
+// let johnHeight = 1.95;
+
+// const mark_BMI = markWeight / markHeight ** 2;
+// const john_BMI = johnWeight / johnHeight ** 2;
+
+// console.log(mark_BMI, john_BMI);
+
+// let markHigherBMI = mark_BMI > john_BMI;
+// console.log(markHigherBMI);
+
+// Test Data 2
+/*
+let markWeight = 95;
+let markHeight = 1.88;
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+const mark_BMI = markWeight / markHeight ** 2;
+const john_BMI = johnWeight / johnHeight ** 2;
+
+console.log(mark_BMI, john_BMI);
+
+let markHigherBMI = mark_BMI > john_BMI;
+console.log(markHigherBMI);
+*/
+
+// String And Template Literals
+
+const firstName = "Pooja";
+const job = "software Engineer";
+const birthYear = 1997;
+const currentYear = 2023;
+
+const result =
+  "I'm " +
+  firstName +
+  " , " +
+  " a" +
+  " " +
+  (currentYear - birthYear) +
+  " year old " +
+  job;
+console.log(result); // writing string this way is hectic instead of doing this we can use Template literals which is much more easier
+
+// Template literals -> `` using back sticks we can write string . ${} -> place holders
+
+const poojaNew = `I'm ${firstName}, a ${
+  currentYear - birthYear
+} year old ${job}!`;
+console.log(poojaNew);
+
+console.log(`Just a regular string...`);
+console.log(
+  "Multiple line \n\
+string "
+);
+
+console.log(`Multiple line
+strings
+!`);
