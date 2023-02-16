6b4f79f27ececc57ab5cc3e9f83f8bf3639b4e79
PYadav19 <poojaydm9@gmail.com>
2023-02-16 18:24:28 +0530

    coding challenges

--- script.js
+++ script.js
@@ -139,3 +139,130 @@ string "
 console.log(`Multiple line
 strings
 !`);
+
+// Coding challenge 2
+
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 92;
+// let johnHeight = 1.95;
+
+let markWeight = 95;
+let markHeight = 1.88;
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+const mark_BMI = markWeight / markHeight ** 2;
+const john_BMI = johnWeight / johnHeight ** 2;
+
+console.log(mark_BMI, john_BMI);
+
+let markHigherBMI = mark_BMI > john_BMI;
+console.log(markHigherBMI);
+
+if (mark_BMI > john_BMI) {
+  console.log("Mark's BMI is Higher than John's");
+} else {
+  console.log("John's BMI is Higher than Mark's");
+}
+
+// Using template literals
+if (mark_BMI > john_BMI) {
+  console.log(
+    `Mark's BMI (${mark_BMI}) is higher than John's BMI ${john_BMI} `
+  );
+} else {
+  console.log(
+    `John's BMI (${john_BMI}) is higher than Mark's BMI ${mark_BMI} `
+  );
+}
+
+// Coding challenge #3
+
+let dolphin_Match1_Score = 70;
+let dolphin_Match2_Score = 100;
+let dolphin_Match3_score = 86;
+
+const dolphin_Avg =
+  (dolphin_Match1_Score + dolphin_Match2_Score + dolphin_Match3_score) / 3;
+console.log(dolphin_Avg);
+
+let koalas_Match1_Score = 96;
+let koalas_Match2_Score = 108;
+let koalas_Match3_score = 86;
+
+const koalas_Avg =
+  (koalas_Match1_Score + koalas_Match2_Score + koalas_Match3_score) / 3;
+console.log(koalas_Avg);
+
+if (dolphin_Avg > koalas_Avg) {
+  console.log("Dolphin is the winner of the Match");
+} else if (dolphin_Avg === koalas_Avg) {
+  console.log("Draw....!!");
+} else {
+  console.log("Koalas is the winner of the Match");
+}
+
+// Bonus1
+
+let dolphin_Match1Score = 97;
+let dolphin_Match2Score = 112;
+let dolphin_Match3score = 101;
+
+const dolphinAvg =
+  (dolphin_Match1_Score + dolphin_Match2_Score + dolphin_Match3_score) / 3;
+console.log(dolphin_Avg);
+
+let koalas_Match1Score = 109;
+let koalas_Match2Score = 95;
+let koalas_Match3score = 106;
+
+const koalasAvg =
+  (koalas_Match1_Score + koalas_Match2_Score + koalas_Match3_score) / 3;
+console.log(koalas_Avg);
+
+const minimum_score = 100;
+if (dolphin_Avg > koalas_Avg && dolphin_Avg >= minimum_score) {
+  console.log("Dolphin is the winner of the Match");
+} else if (koalas_Avg > dolphin_Avg && koalas_Avg >= minimum_score) {
+  console.log("Koalas is the winner of the Match");
+} else if (
+  dolphin_Avg === koalas_Avg &&
+  dolphin_Avg >= minimum_score &&
+  koalas_Avg >= minimum_score
+) {
+  console.log("Draw....!!");
+} else {
+  console.log("No Team wins the trophy");
+}
+
+//Coding chanllenge #4  - Tip calculator
+let tip;
+let totalBill;
+let stevenBill = 275;
+if (stevenBill >= 50 && stevenBill <= 300) {
+  tip = 0.15;
+  totalTip = tip * stevenBill;
+  console.log(
+    `The Bill was ${stevenBill}, the tip was ${totalTip}, The total Bill value ${
+      stevenBill + totalTip
+    }`
+  );
+} else {
+  tip = 0.2;
+  totalTip = tip * stevenBill;
+  console.log(
+    `The Bill was ${stevenBill}, the tip was ${totalTip}, The total Bill value ${
+      stevenBill + totalTip
+    }`
+  );
+}
+
+// ternary operator
+tip =
+  stevenBill >= 50 && stevenBill <= 300 ? stevenBill * 0.15 : stevenBill * 0.2;
+console.log(
+  `The Bill was ${stevenBill}, the tip was ${tip} and the bill Value is ${
+    stevenBill + tip
+  }`
+);
