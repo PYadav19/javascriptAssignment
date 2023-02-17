0c3ea4d9bf9af731a81df6b61565fe1c9269d4ae
PYadav19 <poojaydm9@gmail.com>
2023-02-17 17:46:10 +0530

    new

---
+++ script1.js
@@ -0,0 +1,11 @@
+"use strict";
+let hasDriversLicense = False;
+const passTest = True;
+
+if (passTest) hasDriversLicense = false;
+if (hasDriversLicense) console.log("you can drive...");
+
+// Strict mode help us to avoid error
+
+// Functions
+//functions are basically piece of code that we can use over and over again in our Code.
