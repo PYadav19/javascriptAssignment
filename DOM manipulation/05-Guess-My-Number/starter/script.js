28054289b660c665c906bf56cc52dbaf170df026
PYadav19 <poojaydm9@gmail.com>
2023-02-25 21:41:33 +0530

    editing

--- DOM manipulation/05-Guess-My-Number/starter/script.js
+++ DOM manipulation/05-Guess-My-Number/starter/script.js
@@ -1,2 +1,9 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 12;
+document.querySelector('.score').textContent = 23;
+
+document.querySelector('.guess').Value = 10;
+console.log(document.querySelector('.guess').Value);
