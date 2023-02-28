ded26769b6f0138bb876c4dfaa75284691f09466
PYadav19 <poojaydm9@gmail.com>
2023-02-28 17:14:52 +0530

    guess my number project

--- DOM manipulation/05-Guess-My-Number/starter/script.js
+++ DOM manipulation/05-Guess-My-Number/starter/script.js
@@ -9,13 +9,38 @@ document.querySelector('.score').textContent = 23;
 document.querySelector('.guess').Value = 10;
 console.log(document.querySelector('.guess').Value);
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+/*document.querySelector('.number').textContent = secretNumber;*/
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  /*console.log(guess, typeof guess);  */
 
   if (!guess) {
-    console.log(
-      (document.querySelector('.message').textContent = '⛔No Number')
-    );
+    document.querySelector('.message').textContent = '⛔No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' You Won 🏆';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Too high📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' You lost the game ☹☹';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Too high📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' You lost the game ☹☹';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
