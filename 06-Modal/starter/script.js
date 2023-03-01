2352717a0f00a3eeb72c4def4afc63fe2449b92f
PYadav19 <poojaydm9@gmail.com>
2023-03-01 17:15:54 +0530

    classlist

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -11,4 +11,14 @@ for (let i = 0; i <= openModal.length; i++) {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
+
+  closeModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+
+  overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
 }
