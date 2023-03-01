d6dee44f4026a027a1f22302959b46c641584888
PYadav19 <poojaydm9@gmail.com>
2023-03-01 16:36:52 +0530

    New project

---
+++ 06-Modal/starter/script.js
@@ -0,0 +1,14 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+
+console.log(openModal);
+
+for (let i = 0; i <= openModal.length; i++) {
+  openModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
