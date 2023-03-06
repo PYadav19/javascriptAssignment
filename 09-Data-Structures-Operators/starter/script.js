1c1c83c06f72615f28f111423b5a825c3b6e87e4
PYadav19 <poojaydm9@gmail.com>
2023-03-06 16:32:01 +0530

    spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -16,51 +16,141 @@ const restaurant = {
     return this.starterMenu[starterIndex], this.mainMenu[mainIndex];
   },
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  // },
-  // },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    address,
+    time = '20:00',
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
-// Array destructuring
+// spread operator(...)
+
+const arr = [1, 2, 3];
+
+//I want to create a new array by adding 2 new values in the starting of the arr - array .
+
+// manually
+const newBadArray = [4, 5, arr[0], arr[1], arr[2]];
+console.log(newBadArray);
+
+// using spread operator
+const newArray = [6, 7, ...arr];
+console.log(newArray);
+// putting values separately as below
+console.log(...newArray); // output 6 7 1 2 3
+
+// manually
+console.log(1, 2, 3, 4, 5);
+
+// supoose i want to add new main menu by adding one more food item in the existing mainmenu
+const newMainMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMainMenu);
+
+// 2 imp usecases of spread operator 1- shallow copy or arrays and to merge to array
+
+// copy array
+
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// add 2 arrays
+
+const menu = [...restaurant.mainMenu, ...newMainMenu];
+console.log(menu);
+
+/*
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole , 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole , 21',
+  starterIndex: 1,
+});
+
+// Destructuring objects---------------------------------------------------------------------------
+// we have to mention the as it is name of the properties same as in object
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// if you want to give different names to the properties do as below
+const {
+  name: restaurantName,
+  openingHours: Hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, Hours, tags);
+
+// default values
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
+
+// Mutate Value
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 11 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested object
+
+const {
+  fri: { open: O, close: C },
+} = openingHours;
+console.log(O, C);
+*/
+// Array destructuring -----------------------------------------------------------------------
 
 //  without destructuring normal way
-const arr = [2, 4, 6];
-let a = arr[0];
-let b = arr[1];
-let c = arr[2];
-console.log(a, b, c);
+// const arr = [2, 4, 6];
+// let a = arr[0];
+// let b = arr[1];
+// let c = arr[2];
+// console.log(a, b, c);
 
 //desturcturing
-const [x, y, z] = arr;
-console.log(x, y, z);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
 
 // const [first, , second] = restaurant.categories;
 // console.log(first, second);
 
-let [main, secondary] = restaurant.categories;
-console.log(main, secondary);
+// let [main, secondary] = restaurant.categories;
+// console.log(main, secondary);
 
 // Switching variables
-let temp = main;
-main = secondary;
-secondary = temp;
+// let temp = main;
+// main = secondary;
+// secondary = temp;
 
-console.log(main, secondary);
+// console.log(main, secondary);
 
 // using destructuring
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
