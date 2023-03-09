acaff66fb97aac9d0916edf95914fd74f67e69cf
PYadav19 <poojaydm9@gmail.com>
2023-03-09 17:22:42 +0530

    logical operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -277,3 +277,39 @@ console.log(guest);
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
 console.log('Pooja Yadav');
+
+// Logical Assignment operators
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+console.log(rest1);
+console.log(rest2);
+
+// rest1.owner = rest1.owner && '<ANONYMOUS';
+// rest2.owner = rest2.owner && 'ANONYMOUS';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
