8fe9631dd728f783341c481fe8d45a4bb5e4ea60
PYadav19 <poojaydm9@gmail.com>
2023-03-20 16:13:46 +0530

    passing arguments :values and reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -15,3 +15,33 @@ const createBooking = function (flightNum, numPassenger, price) {
 createBooking('LH123');
 createBooking('lh123', 2, 800);
 createBooking('LH123', 2);
+
+//
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas',
+  passport: 235784994764,
+};
+
+const checkIn = function (flightNumber, passenger) {
+  flightNumber = 'LH564';
+  passenger.name = 'MR.' + passenger.name;
+
+  if (passenger.passport === 235784994764) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
